47f02d0602120d24ba24e2aea2fab12c31953cba
jfurdzik <88293361+jfurdzik@users.noreply.github.com>
2023-11-05 02:56:33 -0500

    show all database entries on get page

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import sqlite3
 from flask import Flask, render_template 
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 @app.route("/")
@@ -16,6 +21,11 @@ def indexLink():
 
 @app.route("/get.html")
 def get():
-    return render_template('get.html')
+    conn = get_db_connection()
+    #entry = conn.execute('SELECT * FROM entries WHERE id IN (SELECT id FROM entries ORDER BY RANDOM() LIMIT 1)').fetchone()
+    #entry = 'hey'
+    entry = conn.execute('SELECT * FROM entries').fetchall()
+    conn.close()
+    return render_template('get.html', entry=entry)
 
 app.run(host="localhost", port="8080", debug=True)
