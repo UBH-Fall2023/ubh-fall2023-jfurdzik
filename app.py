8b4fd9400aac8bd287df055540f0d8b8d2f13dfc
jfurdzik <88293361+jfurdzik@users.noreply.github.com>
2023-11-05 02:03:22 -0500

    restructured and relinked all imgs and css for all

--- app.py
+++ app.py
@@ -6,4 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/add.html")
+def add():
+    return render_template('add.html')
+
+@app.route("/index.html")
+def indexLink():
+    return render_template('index.html')
+
+@app.route("/get.html")
+def get():
+    return render_template('get.html')
+
 app.run(host="localhost", port="8080", debug=True)
