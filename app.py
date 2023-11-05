bdcb3482b0d3dde5360d02ab43572e9fed2faa02
jfurdzik <88293361+jfurdzik@users.noreply.github.com>
2023-11-05 01:43:53 -0500

    backend

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 app.run(host="localhost", port="8080", debug=True)
